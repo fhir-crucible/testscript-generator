d408478254dc482e4900400ef2957308db4e012b
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-06-24 00:42:30 -0400

    update IG object class to store content by its type

--- driver.rb
+++ driver.rb
@@ -1,11 +1,17 @@
 require './generator'
-require './TestScriptBuilder'
+# <<<<<<< HEAD
+# require './TestScriptBuilder'
 
-scriptBuilder = TestScriptBuilder.new
-builder = WorkflowBuilder.new
+# scriptBuilder = TestScriptBuilder.new
+# builder = WorkflowBuilder.new
 
-flow = builder.build(test: 'create')
-script = scriptBuilder.build(flow)
+# flow = builder.build(test: 'create')
+# script = scriptBuilder.build(flow)
 
-# generator = Generator.new
-# generator.generate_scripts
+# # generator = Generator.new
+# # generator.generate_scripts
+# =======
+require './IG'
+
+generator = Generator.new
+generator.generate_scripts
