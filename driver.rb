67a02d8b2443af305680a391fb3806732c72d01a
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-09-15 17:27:41 -0400

    Refactor IG Storage (#3)
    * update IG object class to store content by its type
    
    * add @interactions and @rest attributes
    
    * update for IGExtractor inclusion in IG
    
    * rubocop
    
    * Update README.md
    
    Co-authored-by: Jason Walonoski <jawalonoski@users.noreply.github.com>
    
    * replace dev/null w/ nil
    
    * store code_systems and value_sets in hashes
    
    * update url of generated scripts
    
    * support path to igs
    
    * Update README with cli ig_directory support
    
    Co-authored-by: Jason Walonoski <jawalonoski@users.noreply.github.com>

--- driver.rb
+++ driver.rb
@@ -1,11 +1,18 @@
 require './generator'
-require './TestScriptBuilder'
+# <<<<<<< HEAD
+# require './TestScriptBuilder'
 
-scriptBuilder = TestScriptBuilder.new
-builder = WorkflowBuilder.new
+# scriptBuilder = TestScriptBuilder.new
+# builder = WorkflowBuilder.new
 
-flow = builder.build(test: 'create')
-script = scriptBuilder.build(flow)
+# flow = builder.build(test: 'create')
+# script = scriptBuilder.build(flow)
 
-# generator = Generator.new
-# generator.generate_scripts
+# # generator = Generator.new
+# # generator.generate_scripts
+# =======
+require './IG'
+
+igs_directory = ARGV.first || "#{Dir.getwd}/igs"
+generator = Generator.new(igs_directory)
+generator.generate_scripts
--- generator.rb
+++ generator.rb
@@ -1,70 +1,63 @@
+# frozen_string_literal: true
+
 # TODO: Naming and organization clean-up
 require 'zlib'
 require 'zip'
-require 'package'
 require 'json'
 require 'crack'
 require 'fhir_models'
 require 'pry-nav'
 require 'SecureRandom'
-require_relative './IGExtractor'
+require_relative './IG'
 require_relative './TestScriptWorkflow'
 require_relative './TestScriptGenerator'
 
 class Generator
-  include IGExtractor
+  attr_accessor :igs_directory
 
   MAPPING = {
     'read' => 'read',
     'create' => 'create',
     'delete' => 'delete',
     'search-type' => 'search'
-  }
+  }.freeze
 
-  def igs 
-    @igs ||= load_igs
-  end 
+  def initialize(path)
+    self.igs_directory = path
+  end
 
-  def interactions_map
-    @interactions_map ||= map_interactions
-  end 
+  def igs
+    @igs ||= Dir.glob("#{igs_directory}/*").each_with_object({}) do |package, store|
+      ig = IG.new(package)
+      store[ig.name] = ig
+    end
+  end
 
   def root
-    @root ||= "./testscripts/generated"
-  end 
+    @root ||= './testscripts/generated'
+  end
 
-  def title *new_title
-    @title = "#{new_title[0].split('-').each(&:upcase!).join(' ')} #{new_title[1].scan(/[A-Z][a-z]+/).join(' ')} #{new_title[2].capitalize} TestScript" unless new_title.empty?
-    return @title
-  end 
-
-  def map_interactions
-    igs.each_with_object({}) do |ig, igs_map|
-      resources = ig.capability_statement&.resource
-      return unless resources
-
-      igs_map[ig.name] = resources.each_with_object({}) do |resource, resource_map|
-        resource_map[resource.type] = resource.interaction.each_with_object({}) do |interaction, interaction_map|
-          interaction_map[interaction.code] = interaction.extension[0]&.valueCode
-        end
-      end 
+  def title(*new_title)
+    unless new_title.empty?
+      @title = "#{new_title[0].split('-').each(&:upcase!).join(' ')} #{new_title[1].scan(/[A-Z][a-z]+/).join(' ')} #{new_title[2].capitalize} TestScript"
     end
-  end 
+    @title
+  end
 
   def generate_scripts
-    interactions_map.each do |ig_name, interactions|
-      FHIR.logger.info "Generating TestScripts from #{ig_name} IG ...\n"
+    igs.each do |name, ig|
+      FHIR.logger.info "Generating TestScripts from #{name} IG ...\n"
 
-      script_generator = TestScriptGenerator.new(ig_name, interactions)
+      script_generator = TestScriptGenerator.new(name, ig.interactions)
       script_generator.conformance_generation
 
-      FHIR.logger.info "... finished generating TestScripts from #{ig_name} IG.\n"
-    end 
-  end 
+      FHIR.logger.info "... finished generating TestScripts from #{name} IG.\n"
+    end
+  end
 
-  def script_boilerplate action
-    return {
-      url: 'https://gitlab.mitre.org/fhir-foundry/',
+  def script_boilerplate(_action)
+    {
+      url: 'https://github.com/fhir-crucible/testscript-generator',
       version: '0.0',
       id: @title.split(' ').each(&:downcase!).join('-'),
       name: @title,
@@ -72,6 +65,6 @@ class Generator
       experimental: true,
       date: DateTime.now.to_s,
       publisher: 'The MITRE Corporation'
-    } 
-  end 
-end 
+    }
+  end
+end
