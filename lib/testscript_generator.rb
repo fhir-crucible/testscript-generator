5b038756117443ca3472781671bbb53c4b094748
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-10-28 12:40:30 -0700

    Connectathon Clean-Up (#20)
    * update Gemspec + Gemfile + Gemfile.lock + bin/testscript_generator to create gem executable
    
    * organize available IGs
    
    * rubocop on testscript_generator
    
    * update README.md to direct use of bundle exec
    
    * clean-up output naming conventions

--- lib/testscript_generator.rb
+++ lib/testscript_generator.rb
@@ -1,111 +1,135 @@
+# frozen_string_literal: true
 
-require 'rails'
-require 'pry-nav'
 require 'fileutils'
 require_relative 'testscript_generator/ig'
 require_relative 'testscript_generator/workflow_builder'
 require_relative 'testscript_generator/testscript_builder'
 
+require 'pry-nav'
+
+# The TestScriptGenerator class is responsible for guiding generation
+# depending on intent. While right now the generator can only generate
+# basic 'interaction' tests, in the future it will be responsible for
+# giving the appropriate inputs to the builder, depending on the generation
+# intent (interaction, basic search, operations etc.)
 class TestScriptGenerator
-	attr_accessor :igs_path, :output_path
+  attr_accessor :igs_path, :output_path
 
-	def workflows
-		@workflows ||= {}
-	end
+  def workflows
+    @workflows ||= {}
+  end
 
-	def scripts
-		@scripts ||= {}
-	end
+  def scripts
+    @scripts ||= {}
+  end
 
-	def workflow_builder
-		@workflow_builder ||= WorkflowBuilder.new
-	end
+  def workflow_builder
+    @workflow_builder ||= WorkflowBuilder.new
+  end
 
-	def script_builder
-		@script_builder ||= TestScriptBuilder.new
-	end
+  def script_builder
+    @script_builder ||= TestScriptBuilder.new
+  end
 
-	def igs
-		@igs ||= Dir.glob(igs_path).each_with_object({}) do |package, store|
-			ig = IG.new(package)
-			store[ig.name] = ig
-		end
-	end
+  def igs
+    @igs ||= Dir["#{igs_path}/**/*"].each_with_object({}) do |package, store|
+      ig = IG.new(package)
+      store[ig.name] = ig
+    end
+  end
 
-	def initialize(igs_path, output_path)
-		@igs_path = igs_path
-		@output_path = output_path
+  def initialize(igs_path, output_path)
+    self.igs_path = igs_path
+    self.output_path = output_path
+  end
 
-		igs_path.concat('/*') if File.directory?(igs_path)
-	end
+  def add_boilerplate(script)
+    script.url = 'https://github.com/fhir-crucible/testscript-generator'
+    script.version = '0.0'
+    script.experimental = true
+    script.status = 'draft'
+    script.publisher = 'The MITRE Corporation'
+  end
 
-	def add_boilerplate(script, script_name)
-		script.url = 'https://github.com/fhir-crucible/testscript-generator'
-		script.version = '0.0'
-		script.name = script_name.split('_').map(&:capitalize).join('')
-		script.title = script_name.split('_').map(&:capitalize).join(' ')
-		script.id = script_name.gsub('_', '-')
-		script.status = 'draft'
-		script.experimental = true
-		script.date = DateTime.now.to_s
-		script.publisher = 'The MITRE Corporation'
-	end
+  def customize_script(script, script_name)
+    name = script_name.split('_').map(&:capitalize)
+    script.name = name.join('')
+    script.title = name.join(' ')
+    script.id = script_name.gsub('_', '-')
+    script.date = DateTime.now.to_s
+  end
 
-	def get_name(ig, resource, verb, interaction)
-		"#{ig}_#{resource}_#{verb}_#{interaction}".gsub("-", "_").downcase!
-	end
+  def assign_script_details(script, script_name)
+    add_boilerplate(script)
+    customize_script(script, script_name)
+  end
 
-	def output_script(script, name)
-		FileUtils.mkdir_p(output_path)
-		File.write("#{output_path}/#{name}.json", script)
-	end
+  def build_name(*input)
+    input.map(&:downcase).join(" ").gsub("-", " ")
+  end
 
-	def output_example(resource)
-		example_resource = "FHIR::#{resource}".constantize.new.to_json
-		FileUtils.mkdir_p("#{output_path}/fixtures")
-		File.write("#{output_path}/fixtures/example_#{resource.downcase}.json", example_resource)
-	end
+  def output_script(path, script, name)
+    File.write("#{path}/#{name}.json", script)
+  end
 
-	def generate_interaction_conformance
-		igs.each do |name, ig|
-			FHIR.logger.info "Generating TestScripts from #{name} IG ...\n"
+  def output_example(path, resource)
+    example_resource = "FHIR::#{resource}".constantize.new.to_json
+    FileUtils.mkdir_p("#{path}/fixtures")
+    File.write("#{path}/fixtures/example_#{resource.downcase}.json", example_resource)
+  end
 
-			['SHALL', 'SHOULD', 'MAY'].each do |target_verb|
-				FHIR.logger.info "	Generating #{target_verb} support tests\n"
+  def make_directory(dir_name)
+    FileUtils.mkdir_p(dir_name)
+  end
 
-				ig.interactions.each do |resource, verbs_map|
-					target_verb_interactions = verbs_map.filter_map do |action, verb|
-						action if verb == target_verb
-					end
+  def generate_interaction_conformance
+    igs.each do |ig_name, ig_contents|
+      FHIR.logger.info "Generating TestScripts from #{ig_name} IG ...\n"
+      ig_directory = "#{output_path}/#{ig_name}"
+      make_directory(ig_directory)
 
-					target_verb_interactions.each do |interaction|
-						if ['create', 'read', 'update', 'delete', 'search-type'].include?(interaction)
+      %w[SHALL SHOULD MAY].each do |conformance_level|
+        FHIR.logger.info "	Generating #{conformance_level} support tests\n"
+        conformance_directory = "#{ig_directory}/#{conformance_level}"
+        make_directory(conformance_directory)
 
-							if workflows[interaction]
-								script = scripts[workflows[interaction]]
-								script_name = get_name(name, resource, target_verb, interaction)
-								add_boilerplate(script, script_name)
-								script = script.to_json.gsub("${RESOURCE_TYPE_1}", resource).gsub("${EXAMPLE_RESOURCE_1}_reference", "example_#{resource.downcase}.json").gsub("${EXAMPLE_RESOURCE_1}", "example_#{resource.downcase}")
-								output_script(script, script_name)
-								output_example(resource)
-							else
-								workflow = workflow_builder.build(test: interaction)
-								workflows[interaction] = workflow
-								scripts[workflow] = script_builder.build(workflow)
-							end
+        ig_contents.interactions.each do |resource, verbs_map|
+          target_verb_interactions = verbs_map.filter_map do |action, verb|
+            action if verb == conformance_level
+          end
 
-							FHIR.logger.info "		Generated tests for #{resource} [#{interaction}]."
-						else
-							FHIR.logger.info "		Skipping test generation for #{resource} [#{interaction}]. Currently only CRUD + Search interactions supported."
-							next
-						end
-					end
-				end
+          target_verb_interactions.each do |interaction|
+            if %w[create read update delete search-type].include?(interaction)
 
-				puts "\n"
-			end
+              if workflows[interaction]
+                script = scripts[workflows[interaction]]
+              else
+                workflow = workflow_builder.build(test: interaction)
+                workflows[interaction] = workflow
+                scripts[workflow] = script_builder.build(workflow)
+                script = scripts[workflow]
+              end
 
-			FHIR.logger.info "... finished generating TestScripts from #{name} IG.\n"
-		end
-	end
+              script_name = build_name(ig_name, conformance_level, interaction, resource)
+              assign_script_details(script, ig_name)
+              script = script.to_json.gsub('${RESOURCE_TYPE_1}', resource).gsub('${EXAMPLE_RESOURCE_1}_reference', "example_#{resource.downcase}.json").gsub(
+                '${EXAMPLE_RESOURCE_1}', "example_#{resource.downcase}"
+              )
+              output_script(conformance_directory, script, script_name)
+              output_example(conformance_directory, resource)
+
+              FHIR.logger.info "		Generated tests for #{resource} [#{interaction}]."
+            else
+              FHIR.logger.info "		Skipping test generation for #{resource} [#{interaction}]. Currently only CRUD + Search interactions supported."
+              next
+            end
+          end
+        end
+
+        puts "\n"
+      end
+
+      FHIR.logger.info "... finished generating TestScripts from #{ig_name} IG.\n"
+    end
+  end
 end
--- lib/testscript_generator/ig.rb
+++ lib/testscript_generator/ig.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require 'zip'
-require 'fhir_models'
+require 'fhir_client'
 require 'rubygems/package'
 
 class IG
