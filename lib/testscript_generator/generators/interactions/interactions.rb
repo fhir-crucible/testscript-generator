de0afb3c39e0ec854e38816de027d008ebbeb916
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-10-31 07:19:33 -0700

    initialization of interactions class

---
+++ lib/testscript_generator/generators/interactions/interactions.rb
@@ -0,0 +1,36 @@
+require 'yaml'
+
+# What is a dynamic requirement? It's something that needs to be known, specifically, in order
+# to access a specific resource on the endpoint
+
+module Interactions
+  def interactions
+    base_interactions
+  end
+
+  def base_interactions
+    @base_interactions ||= begin
+      path = "lib/testscript_generator/generators/interactions/interactions_base.yml"
+      YAML.load(File.read(Dir[path].first))
+    end
+  end
+
+  def method(interaction)
+    interactions[interaction]
+  end
+
+  def requires_setup?(interaction)
+    base_interactions[interaction]["dynamic_reqs"].present?
+  end
+
+  def dynamic_requirement?(interaction)
+    interactions[interaction]
+  end
+
+  def requires_id?(interaction)
+    interactions[interaction].dynamic_requirements
+  end
+
+  def requires_type?(interaction)
+  end
+end
